82b81fc6217378d616dda2e1a4493f5d63383c1f
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-02-03 10:58:29 -0800

    pass test 3

--- src/AgeCalc.js
+++ src/AgeCalc.js
@@ -8,9 +8,9 @@ export default class GalacticAge {
         this.mercuryAge = Math.round(mercuryAge);
     }
 
-   vensusAge() {
-        // let mercuryAge = this.userAge / .24;
-        // this.mercuryAge = Math.round(mercuryAge);
+    vensusAge() {
+        let vensusAge = this.userAge / .62;
+        this.vensusAge = Math.round(vensusAge);
     }
 
 
