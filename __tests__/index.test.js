283ec89c8c329a7f6a070bc91edc5d7f44b722e2
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-02-03 10:40:58 -0800

    updated test 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,7 @@ import GalacticAge from './../src/AgeCalc.js';
 
 describe('GalacticAge', () => {
 
-    test("Should return user age which will then be used to calculate everything else.", () => {
+    test("Should return user age", () => {
       const userAge = new GalacticAge(25);
       expect(userAge.userAge).toEqual(25);
 
