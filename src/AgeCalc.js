30bd920cf9ef8674752f025087d19dffcd7dc5ba
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-02-03 11:39:14 -0800

    updated formating on agecalc

--- src/AgeCalc.js
+++ src/AgeCalc.js
@@ -2,18 +2,14 @@ export default class GalacticAge {
     constructor(userAge) {
         this.userAge = userAge;
     }
-
     mercuryAge() {
         let mercuryAge = this.userAge / .24;
         this.mercuryAge = Math.round(mercuryAge);
     }
-
     vensusAge() {
         let vensusAge = this.userAge / .62;
         this.vensusAge = Math.round(vensusAge);
     }
-
-
     marsAge() {
         let marsAge = this.userAge / 1.88;
         this.marsAge = Math.round(marsAge);
@@ -22,4 +18,7 @@ export default class GalacticAge {
         let jupiterAge = this.userAge / 11.86;
         this.jupiterAge = Math.round(jupiterAge);
     }
+
+    
+
 }
